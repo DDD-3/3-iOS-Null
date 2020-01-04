24fb7ae512137d572242001149dcfc9e01dc72dd
zunzunzun <zun8ang@gmail.com>
2020-01-04 15:49:32 +0900

    first commit

---
+++ MovieConnect/MovieConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieConnect
+//
+//  Created by zun on 04/01/2020.
+//  Copyright © 2020 zun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
